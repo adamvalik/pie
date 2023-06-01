ffdd4472f3a69624c5f57d4acc5aa10e99ed5f47
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-06-01 17:16:05 +0200

    Update mainwindow.py
    settings: colors, show percentage

--- mainwindow.py
+++ mainwindow.py
@@ -1,36 +1,110 @@
 import os
 import matplotlib.pyplot as plt
-from PySide6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QPushButton, QDialog, QColorDialog
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QPushButton, QDialog, QColorDialog, QCheckBox
+from PySide6.QtCore import Qt, Signal
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PySide6.QtGui import QFont, QFontDatabase
 
 
 class SettingsWindow(QDialog):
+    save_clicked = Signal(str, str, bool)
+
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.setWindowTitle("Settings")
-
+        self.setMinimumSize(300, 200)
+        self.setMaximumSize(600, 400)
 
         layout = QVBoxLayout()
+        color1_layout = QHBoxLayout()
+        color2_layout = QHBoxLayout()
+        checkbox_layout = QHBoxLayout()
 
-        self.color1_label = QLabel("Color 1:")
+        self.color1_label = QLabel("Done:")
         self.color1_edit = QLineEdit()
         self.color1_button = QPushButton("Select Color")
         self.color1_button.clicked.connect(self.select_color1)
 
-        self.color2_label = QLabel("Color 2:")
+        self.color2_label = QLabel("TBD: ")
         self.color2_edit = QLineEdit()
         self.color2_button = QPushButton("Select Color")
         self.color2_button.clicked.connect(self.select_color2)
 
-        layout.addWidget(self.color1_label)
-        layout.addWidget(self.color1_edit)
-        layout.addWidget(self.color1_button)
-        layout.addWidget(self.color2_label)
-        layout.addWidget(self.color2_edit)
-        layout.addWidget(self.color2_button)
+        self.checkbox_label = QLabel("Show percentage:")
+        self.show_percentage = QCheckBox()
+        
+        #####################################
+        self.show_percentage.setChecked(True)
+        #####################################
+
+        self.save_button = QPushButton("Save settings")
+        self.save_button.clicked.connect(self.save_settings)
+
+        self.setStyleSheet("""
+            QLabel {
+                font-size: 15px;
+                color: #FFFFFF;
+            }
+            QLineEdit {
+                padding: 2px;
+                font-size: 15px;
+                border: 1px solid #FFFFFF;
+                border-radius: 4px;
+                background-color: #333333;
+                selection-color: yellow;
+                selection-background-color: blue;
+            }
+            QPushButton {
+                padding: 6px 12px;
+                font-size: 15px;
+                border: 1px solid #FFFFFF;
+                border-radius: 4px;
+                background-color: #286090;
+                color: #FFFFFF;
+            }
+            QPushButton:hover {
+                background-color: #1A4D73;
+            }
+            QPushButton:pressed {
+                background-color: #144057;
+            }
+        """)
+
+        # Load and set the font
+        font_path = os.path.join(os.path.dirname(__file__), "Montserrat-Regular.ttf")
+        font_id = QFontDatabase.addApplicationFont(font_path)
+        font_families = QFontDatabase.applicationFontFamilies(font_id)
+
+        # Check if the font file was loaded successfully
+        if font_id != -1 and font_families:
+            font_family = font_families[0]
+
+            # Set the font of the QLabel objects
+            font = QFont(font_family)
+            self.color1_label.setFont(font)
+            self.color1_edit.setFont(font)
+            self.color1_button.setFont(font)
+            self.color2_label.setFont(font)
+            self.color2_edit.setFont(font)
+            self.color2_button.setFont(font)
+            self.save_button.setFont(font)
+            self.checkbox_label.setFont(font)
+
+
+        color1_layout.addWidget(self.color1_label)
+        color1_layout.addWidget(self.color1_edit)
+        color1_layout.addWidget(self.color1_button)
+        color2_layout.addWidget(self.color2_label)
+        color2_layout.addWidget(self.color2_edit)
+        color2_layout.addWidget(self.color2_button)
+        checkbox_layout.addWidget(self.checkbox_label)
+        checkbox_layout.addWidget(self.show_percentage)
+
+        layout.addLayout(color1_layout)
+        layout.addLayout(color2_layout)
+        layout.addLayout(checkbox_layout)
+        layout.addWidget(self.save_button)
 
         self.setLayout(layout)
 
@@ -45,6 +119,22 @@ class SettingsWindow(QDialog):
         color = color_dialog.getColor()
         if color.isValid():
             self.color2_edit.setText(color.name())
+
+
+    def save_settings(self):
+        color1 = self.color1_edit.text()
+        color2 = self.color2_edit.text()
+        state = self.show_percentage.isChecked()
+
+        self.save_clicked.emit(color1, color2, state)
+        self.close()
+
+
+
+
+
+
+
 
 
 class MainWindow(QMainWindow):
@@ -65,11 +155,10 @@ class MainWindow(QMainWindow):
         # Create input fields
         input1_label = QLabel("Total:")
         self.input1_edit = QLineEdit()
+        self.input1_edit.textEdited.connect(self.update_chart)
 
         input2_label = QLabel("Current:")
         self.input2_edit = QLineEdit()
-
-        self.input1_edit.textEdited.connect(self.update_chart)
         self.input2_edit.textEdited.connect(self.update_chart)
 
         # Create settings button
@@ -87,16 +176,26 @@ class MainWindow(QMainWindow):
                 font-size: 15px;
                 border: 1px solid #FFFFFF;
                 border-radius: 4px;
+                background-color: #333333;
+                selection-color: yellow;
+                selection-background-color: blue;
             }
             QPushButton {
-                padding: 2px 8px;
-                font-size: 15px;
+                padding: 6px 12px;
+                font-size: 10px;
                 border: 1px solid #FFFFFF;
                 border-radius: 4px;
                 background-color: #286090;
                 color: #FFFFFF;
             }
+            QPushButton:hover {
+                background-color: #1A4D73;
+            }
+            QPushButton:pressed {
+                background-color: #144057;
+            }
         """)
+
 
         # Create a canvas for the matplotlib chart
         self.figure = plt.figure()
@@ -107,9 +206,9 @@ class MainWindow(QMainWindow):
         input_layout.addWidget(self.input1_edit)
         input_layout.addWidget(input2_label)
         input_layout.addWidget(self.input2_edit)
+        input_layout.addWidget(self.settings_button)
 
         layout.addLayout(input_layout)
-        layout.addWidget(self.settings_button)
         layout.addWidget(self.canvas)
 
         self.setCentralWidget(main_widget)
@@ -138,12 +237,17 @@ class MainWindow(QMainWindow):
         settings_window = SettingsWindow(self)
         settings_window.color1_edit.setText(self.color1)
         settings_window.color2_edit.setText(self.color2)
-        if settings_window.exec_():
-            self.color1 = settings_window.color1_edit.text()
-            self.color2 = settings_window.color2_edit.text()
-            self.update_chart()
+        settings_window.save_clicked.connect(self.handle_settings_saved)
+        settings_window.exec_()
 
-    def update_chart(self):
+    
+    def handle_settings_saved(self, color1, color2, state):
+        self.color1 = color1
+        self.color2 = color2
+        self.update_chart(state)
+
+
+    def update_chart(self, show_percentage):
         # Get the input values from the input fields
         input1 = float(self.input1_edit.text())
         input2 = float(self.input2_edit.text())
@@ -160,7 +264,10 @@ class MainWindow(QMainWindow):
 
         # Create the pie chart using Matplotlib
         ax = self.figure.add_subplot(111)
-        ax.pie(values, colors=[self.color1, self.color2], autopct='%1.1f%%', wedgeprops={"linewidth": 1, "edgecolor": "white"})
+        if show_percentage:
+            ax.pie(values, colors=[self.color1, self.color2], autopct='%1.1f%%', wedgeprops={"linewidth": 1, "edgecolor": "white"})
+        else:
+            ax.pie(values, colors=[self.color1, self.color2], wedgeprops={"linewidth": 1, "edgecolor": "white"})
         ax.axis('equal')
 
         # Update the canvas
